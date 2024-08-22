bdb568a2efa80ce0aa39e3a296797c7cd8b746b0
Alex Richards <alex-richards@users.noreply.github.com>
2024-08-22 14:53:01 +1200

    reader auth

--- reader_auth.go
+++ reader_auth.go
@@ -1,8 +1,10 @@
 package mdoc
 
 import (
+	"bytes"
 	"crypto/ecdsa"
 	"crypto/x509"
+	"encoding/asn1"
 	"errors"
 	"time"
 
@@ -11,9 +13,11 @@ import (
 )
 
 var (
-	ErrMissingAlgorithmHeader = errors.New("missing algorithm header")
-	ErrUnsupportedAlgorithm   = errors.New("unsupported algorithm")
-	ErrEmptyChain             = errors.New("empty chan")
+	ErrMissingAlgorithmHeader       = errors.New("missing algorithm header")
+	ErrUnsupportedAlgorithm         = errors.New("unsupported algorithm")
+	ErrNoRootCertificates           = errors.New("no root certificates")
+	ErrEmptyChain                   = errors.New("empty chan")
+	ErrInvalidReaderAuthCertificate = errors.New("invalid reader auth certificate")
 )
 
 type ReaderAuth cose.UntaggedSign1Message
@@ -50,33 +54,54 @@ func (ra *ReaderAuth) Verify(
 	if err != nil {
 		return err
 	}
+	if len(chain) == 0 {
+		return ErrNoRootCertificates
+	}
 
 	chainLen := len(chain)
 	if chainLen == 0 {
 		return ErrEmptyChain
 	}
 
-	firstCertificate := chain[0]
-	for _, rootCertificate := range rootCertificates {
-		err = firstCertificate.CheckSignatureFrom(rootCertificate)
-		if err == nil {
-			break // found our root cert
+	var rootCertificate *x509.Certificate
+	var previousCertificate *x509.Certificate
+
+	{
+		firstCertificate := chain[0]
+		for _, candidateRootCertificate := range rootCertificates {
+			if err = checkReaderAuthIntermediateCertificate(now, firstCertificate, candidateRootCertificate); err == nil {
+				rootCertificate = candidateRootCertificate
+				break
+			}
 		}
-	}
-	if err != nil {
-		return err
+		if rootCertificate == nil {
+			return ErrInvalidReaderAuthCertificate
+		}
+		previousCertificate = firstCertificate
 	}
 
-	signingCertificate := firstCertificate
-	for _, certificate := range chain[1:] {
-		err = certificate.CheckSignatureFrom(signingCertificate)
-		if err != nil {
+	{
+		for _, certificate := range chain[1:] {
+			err = checkReaderAuthIntermediateCertificate(now, certificate, previousCertificate)
+			if err != nil {
+				return err
+			}
+			previousCertificate = certificate
+		}
+	}
+
+	readerAuthCertificate := previousCertificate
+	if chainLen == 1 {
+		if err = checkReaderAuthCertificate(readerAuthCertificate, rootCertificate); err != nil {
 			return err
 		}
-		signingCertificate = certificate
+	} else {
+		if err = checkReaderAuthCertificate(readerAuthCertificate, chain[chainLen-2]); err != nil {
+			return err
+		}
 	}
 
-	publicKey, ok := signingCertificate.PublicKey.(*ecdsa.PublicKey)
+	publicKey, ok := readerAuthCertificate.PublicKey.(*ecdsa.PublicKey)
 	if !ok {
 		return ErrUnsupportedAlgorithm
 	}
@@ -98,6 +123,85 @@ func (ra *ReaderAuth) Verify(
 	if err != nil {
 		return err
 	}
+
+	return nil
+}
+
+func checkReaderAuthIntermediateCertificate(now time.Time, certificate *x509.Certificate, signer *x509.Certificate) error {
+	// issuer matches signer's subject
+	if !bytes.Equal(certificate.RawIssuer, signer.RawSubject) {
+		return ErrInvalidReaderAuthCertificate
+	}
+
+	// cert valid
+	if now.After(certificate.NotAfter) {
+		return ErrInvalidReaderAuthCertificate
+	}
+	if now.Before(certificate.NotBefore) {
+		return ErrInvalidReaderAuthCertificate
+	}
+
+	// cert validity is within signer's validity
+	if signer.NotAfter.Before(certificate.NotAfter) {
+		return ErrInvalidReaderAuthCertificate
+	}
+	if signer.NotBefore.After(certificate.NotBefore) {
+		return ErrInvalidReaderAuthCertificate
+	}
+
+	// signature valid
+	err := certificate.CheckSignatureFrom(signer)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func checkReaderAuthCertificate(certificate *x509.Certificate, signer *x509.Certificate) error {
+	if certificate.Version != 3 {
+		return ErrInvalidReaderAuthCertificate
+	}
+
+	// TODO serial number, max len 20 octets
+
+	validityDuration := certificate.NotAfter.Sub(certificate.NotBefore)
+	if validityDuration.Hours()/24 > 1187 {
+		return ErrInvalidReaderAuthCertificate
+	}
+
+	if len(certificate.RawSubject) == 0 {
+		return ErrInvalidReaderAuthCertificate
+	}
+
+	// TODO subject public key info checks
+
+	if !bytes.Equal(certificate.AuthorityKeyId, signer.SubjectKeyId) {
+		return ErrInvalidReaderAuthCertificate
+	}
+
+	// TODO subject key identifier check
+
+	// TODO exclusive?
+	if certificate.KeyUsage != x509.KeyUsageDigitalSignature {
+		return ErrInvalidReaderAuthCertificate
+	}
+
+	// TODO issuer alt name
+
+	extKeyUsage := certificate.UnknownExtKeyUsage
+	if len(extKeyUsage) == 0 {
+		return ErrInvalidReaderAuthCertificate
+	}
+	if !extKeyUsage[0].Equal(asn1.ObjectIdentifier{1, 0, 18013, 5, 1, 6}) {
+		return ErrInvalidReaderAuthCertificate
+	}
+
+	// TODO CRL distribution points
+
+	// TODO authority information access
+
+	// TODO signature algorithm
 
 	return nil
 }
